4a2f88221f2bc7749f0b1f43555762e6cf5b29c8
Chun-gu <chungu.yi92@gmail.com>
2022-05-04 23:54:08 +0900

    💡 참고한 코드 출처 표기

--- Silver/4/1158.js
+++ Silver/4/1158.js
@@ -2,12 +2,14 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split(" ").map(Number);
 
 const [n, k] = input;
+// https://stackoverflow.com/questions/3746725/how-to-create-an-array-containing-1-n
 const array = Array.from({ length: n }, (_, v) => v + 1);
 const result = [];
 
 function deleteOneByOne(array, ordinal, targetIndex) {
-  index = targetIndex + k - 1;
+  index = targetIndex + ordinal - 1;
   const length = array.length;
+  // https://dev.to/turneremma21/circular-access-of-array-in-javascript-j52
   targetIndex = ((index % length) + length) % length;
   const deletedNum = array[targetIndex];
   result.push(deletedNum);
